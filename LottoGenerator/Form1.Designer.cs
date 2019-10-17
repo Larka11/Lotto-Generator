92f0906cd6ba431f1fe3494a4eb752910632d007
GLADoS <larkaawolf@gmail.com>
2019-10-17 13:01:27 +0100

    Add project files.

---
+++ LottoGenerator/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace LottoGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lottoLineBox1 = new System.Windows.Forms.TextBox();
+            this.textBox37 = new System.Windows.Forms.TextBox();
+            this.lottoLineBox3 = new System.Windows.Forms.TextBox();
+            this.lottoLineBox6 = new System.Windows.Forms.TextBox();
+            this.lottoLineBox5 = new System.Windows.Forms.TextBox();
+            this.lottoLineBox4 = new System.Windows.Forms.TextBox();
+            this.lottoLineBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(135, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Lotto";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Enabled = false;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 269);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(2, 15);
+            this.label1.TabIndex = 1;
+            // 
+            // lottoLineBox1
+            // 
+            this.lottoLineBox1.Enabled = false;
+            this.lottoLineBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoLineBox1.Location = new System.Drawing.Point(45, 83);
+            this.lottoLineBox1.Name = "lottoLineBox1";
+            this.lottoLineBox1.ReadOnly = true;
+            this.lottoLineBox1.Size = new System.Drawing.Size(264, 20);
+            this.lottoLineBox1.TabIndex = 5;
+            // 
+            // textBox37
+            // 
+            this.textBox37.Enabled = false;
+            this.textBox37.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox37.Location = new System.Drawing.Point(184, 264);
+            this.textBox37.Name = "textBox37";
+            this.textBox37.ReadOnly = true;
+            this.textBox37.Size = new System.Drawing.Size(100, 20);
+            this.textBox37.TabIndex = 41;
+            // 
+            // lottoLineBox3
+            // 
+            this.lottoLineBox3.Enabled = false;
+            this.lottoLineBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoLineBox3.Location = new System.Drawing.Point(45, 135);
+            this.lottoLineBox3.Name = "lottoLineBox3";
+            this.lottoLineBox3.ReadOnly = true;
+            this.lottoLineBox3.Size = new System.Drawing.Size(264, 20);
+            this.lottoLineBox3.TabIndex = 42;
+            // 
+            // lottoLineBox6
+            // 
+            this.lottoLineBox6.Enabled = false;
+            this.lottoLineBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoLineBox6.Location = new System.Drawing.Point(45, 213);
+            this.lottoLineBox6.Name = "lottoLineBox6";
+            this.lottoLineBox6.ReadOnly = true;
+            this.lottoLineBox6.Size = new System.Drawing.Size(264, 20);
+            this.lottoLineBox6.TabIndex = 43;
+            // 
+            // lottoLineBox5
+            // 
+            this.lottoLineBox5.Enabled = false;
+            this.lottoLineBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoLineBox5.Location = new System.Drawing.Point(45, 187);
+            this.lottoLineBox5.Name = "lottoLineBox5";
+            this.lottoLineBox5.ReadOnly = true;
+            this.lottoLineBox5.Size = new System.Drawing.Size(264, 20);
+            this.lottoLineBox5.TabIndex = 44;
+            // 
+            // lottoLineBox4
+            // 
+            this.lottoLineBox4.Enabled = false;
+            this.lottoLineBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoLineBox4.Location = new System.Drawing.Point(45, 161);
+            this.lottoLineBox4.Name = "lottoLineBox4";
+            this.lottoLineBox4.ReadOnly = true;
+            this.lottoLineBox4.Size = new System.Drawing.Size(264, 20);
+            this.lottoLineBox4.TabIndex = 45;
+            // 
+            // lottoLineBox2
+            // 
+            this.lottoLineBox2.Enabled = false;
+            this.lottoLineBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lottoLineBox2.Location = new System.Drawing.Point(45, 109);
+            this.lottoLineBox2.Name = "lottoLineBox2";
+            this.lottoLineBox2.ReadOnly = true;
+            this.lottoLineBox2.Size = new System.Drawing.Size(264, 20);
+            this.lottoLineBox2.TabIndex = 46;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 308);
+            this.Controls.Add(this.lottoLineBox2);
+            this.Controls.Add(this.lottoLineBox4);
+            this.Controls.Add(this.lottoLineBox5);
+            this.Controls.Add(this.lottoLineBox6);
+            this.Controls.Add(this.lottoLineBox3);
+            this.Controls.Add(this.textBox37);
+            this.Controls.Add(this.lottoLineBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Lotto Quick Picker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox lottoLineBox1;
+        private System.Windows.Forms.TextBox textBox37;
+        private System.Windows.Forms.TextBox lottoLineBox3;
+        private System.Windows.Forms.TextBox lottoLineBox6;
+        private System.Windows.Forms.TextBox lottoLineBox5;
+        private System.Windows.Forms.TextBox lottoLineBox4;
+        private System.Windows.Forms.TextBox lottoLineBox2;
+    }
+}
---
+++ LottoGenerator/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace LottoGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                        int i;
+            var numbers = new List<int>();
+            var drawnNumbers = new List<int>();
+
+            //It is important to only have have instance and re-use it. Creating a new one will result in LESS random numbers.
+            var rng = new Random();
+
+            //fill the list Numbers with a for loop here
+            for (i = 1; i < 6; i++)
+            {
+                numbers[i] = rng.Next(1, 49); //Return random numbers seeded between 1 and 49
+            }
+            //draw 7 numbers from the Numbers and put them into DrawnNumbers
+
+            for (i = 0; i < 6; i++)
+            {
+                int randomIndex = rng.Next(0, numbers.Count);
+                var drawnInt = numbers[randomIndex];
+                drawnNumbers.Add(drawnInt);
+                numbers.Remove(drawnInt);
+            }
+
+            i = 1;
+
+            for (i = 1; i < drawnNumbers.Count; i++)
+            {
+                lottoLineBox1.Text += drawnNumbers[i].ToString();
+            }
+        }
+    }
+}
